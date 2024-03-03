f821033b13d9a08040f775ee1a2e12d3d2b1db64
Queen103 <quanhne103@gmail.com>
2024-03-03 19:55:37 +0700

    xử lý xong requirementPage

--- lib/page/homePage.dart
+++ lib/page/homePage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unnecessary_import, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, must_be_immutable, library_private_types_in_public_api, no_logic_in_create_state, unused_field, unused_import
 
+import 'package:ioc_app_demo_1/page/requirementPage.dart';
 import 'package:ioc_app_demo_1/page/testPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -13,7 +14,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   final List<Widget> _page = [
-    ButtonPage(),
+    RequirementPage(),
     FirestoreListPage(),
     UserProfilePage()
   ];
@@ -24,12 +25,12 @@ class _HomeState extends State<Home> {
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.family_restroom_outlined),
-            label: 'Family',
+            icon: Icon(Icons.history),
+            label: 'History',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.portrait_sharp),
